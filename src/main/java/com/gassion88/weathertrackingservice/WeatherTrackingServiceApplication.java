563843efe3222adb78024c2913d09ff2486c2761
Soslan Gassiev <soslangassity04@gmail.com>
2023-07-01 00:56:05 +0300

    feat : add model mapper

--- src/main/java/com/gassion88/weathertrackingservice/WeatherTrackingServiceApplication.java
+++ src/main/java/com/gassion88/weathertrackingservice/WeatherTrackingServiceApplication.java
@@ -1,7 +1,9 @@
 package com.gassion88.weathertrackingservice;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class WeatherTrackingServiceApplication {
@@ -10,4 +12,8 @@ public class WeatherTrackingServiceApplication {
         SpringApplication.run(WeatherTrackingServiceApplication.class, args);
     }
 
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 }
