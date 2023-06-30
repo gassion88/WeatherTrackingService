a63e08b9b819ba81b6214d7b1415cb8d2442346b
Soslan Gassiev <soslangassity04@gmail.com>
2023-06-30 14:44:59 +0300

    init :

---
+++ src/main/java/com/gassion88/weathertrackingservice/WeatherTrackingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gassion88.weathertrackingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherTrackingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherTrackingServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gassion88/weathertrackingservice/WeatherTrackingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gassion88.weathertrackingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherTrackingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
