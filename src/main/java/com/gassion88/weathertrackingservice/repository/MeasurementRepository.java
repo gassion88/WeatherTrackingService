efe13ee122c11a1811d1ff3d14d1e9d43b0a4751
Soslan Gassiev <soslangassity04@gmail.com>
2023-06-30 23:11:33 +0300

    fix :

--- src/main/java/com/gassion88/weathertrackingservice/repository/MeasurementRepository.java
+++ src/main/java/com/gassion88/weathertrackingservice/repository/MeasurementRepository.java
@@ -2,6 +2,8 @@ package com.gassion88.weathertrackingservice.repository;
 
 import com.gassion88.weathertrackingservice.model.Measurment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MeasurementRepository extends JpaRepository<Measurment, Long> {
 }
